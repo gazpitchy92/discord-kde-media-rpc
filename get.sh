b2ac34456ed9da8b03a75d2e017eaf045047cc05
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2025-09-07 23:28:36 +0100

    If set.py dies then restart it - Or on Discord closure

--- get.sh
+++ get.sh
@@ -5,8 +5,14 @@ base_dir="$(dirname "$0")"
 > "$base_dir/playing.txt"
 
 python3 "$base_dir/set.py" &
+pid=$!
 
 while true; do
+    if ! kill -0 $pid 2>/dev/null; then
+        python3 "$base_dir/set.py" &
+        pid=$!
+    fi
+
     artist=$(playerctl metadata --format '{{ artist }}' 2>/dev/null)
     title=$(playerctl metadata --format '{{ title }}' 2>/dev/null)
 
